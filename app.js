7f040a797382992f6b04c302bfbacddccbf95703
eriBloo <19932449+EriBloo@users.noreply.github.com>
2021-01-28 17:33:40 +0100

    Added log in / log out functionality

--- app.js
+++ app.js
@@ -24,11 +24,46 @@ app.set("views", __dirname);
 app.set("view engine", "ejs");
 
 app.use(session({ secret: "cats", resave: false, saveUninitialized: true }));
+
+passport.use(
+  new LocalStrategy((username, password, done) => {
+    User.findOne({ username: username }, (err, user) => {
+      if (err) { 
+        return done(err);
+      };
+      if (!user) {
+        return done(null, false, { msg: "Incorrect username" });
+      }
+      if (user.password !== password) {
+        return done(null, false, { msg: "Incorrect password" });
+      }
+      return done(null, user);
+    });
+  })
+);
+
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+  User.findById(id, function(err, user) {
+    done(err, user);
+  });
+});
+
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(express.urlencoded({ extended: false }));
 
-app.get("/", (req, res) => res.render("index"));
+app.use(function(req, res, next) {
+  res.locals.currentUser = req.user;
+  next();
+});
+
+app.get("/", (req, res) => {
+  res.render("index", { user: req.user });
+});
 app.get("/sign-up", (req, res) => res.render("sign-up-form"));
 app.post("/sign-up", (req, res, next) => {
   const user = new User({
@@ -40,6 +75,17 @@ app.post("/sign-up", (req, res, next) => {
     };
     res.redirect("/");
   });
+});
+app.post(
+  "/log-in",
+  passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/"
+  })
+);
+app.get("/log-out", (req, res) => {
+  req.logout();
+  res.redirect("/");
 });
 
 app.listen(3000, () => console.log("app listening on port 3000!"));
